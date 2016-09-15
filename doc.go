df32ec3d7de386480d27c6b849a73cfa431d7796
Osamu TONOMORI <osamingo@users.noreply.github.com>
2016-09-16 00:21:14 +0900

    Refactor misc tips (#15)
    * Refactor example codes
    
    * Add doc.go

---
+++ doc.go
@@ -0,0 +1,7 @@
+/*
+Package indigo generates a distributed unique ID generator of using Sonyflake and encoded by Base58.
+
+ID max length is 11 characters by unsigned int64 max value.
+Default characters: 123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz
+*/
+package indigo
