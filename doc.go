4673cafa46fbfc27298e9c16b435d411f5c1d8ad
Osamu TONOMORI <osamingo@gmail.com>
2016-10-06 19:55:56 +0900

    Fix golint

--- doc.go
+++ doc.go
@@ -1,7 +1,7 @@
 /*
-	Package indigo generates a distributed unique ID generator of using Sonyflake and encoded by Base58.
-	Base58 logic is optimized unsigned int64.
+Package indigo generates a distributed unique ID generator of using Sonyflake and encoded by Base58.
+Base58 logic is optimized unsigned int64.
 
-	More information: https://github.com/osamingo/indigo/blob/master/README.md
+More information: https://github.com/osamingo/indigo/blob/master/README.md
 */
 package indigo
