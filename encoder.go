8393547f600952e07a98a4e8a29fd48cfc4d6f3f
Osamu TONOMORI <osamingo@users.noreply.github.com>
2016-10-13 17:06:38 +0900

    Add base58 package docs (#24)

--- encoder.go
+++ encoder.go
@@ -2,6 +2,6 @@ package indigo
 
 // An Encoder has Encode and Decode methods.
 type Encoder interface {
-	Encode(id uint64) string
-	Decode(id string) (uint64, error)
+	Encode(uint64) string
+	Decode(string) (uint64, error)
 }
